64610b3cf72410190c86372504c07c853fd775f3
Atharva-K08-OIT <atharvaofficialoit@gmail.com>
2024-09-13 12:00:54 +0530

    Add files via upload

---
+++ 12 Sep/script_12Sep.js
@@ -0,0 +1,13 @@
+// interface:
+// Ex.1
+// interface Gola {
+//   x: number;
+//   y: number;
+//   r: number;
+// }
+var bear = { name: "Bhaalo", honey: true };
+console.log(bear.name);
+console.log(bear.honey);
+var Beear = { name: "Bhaalu", honey: false };
+console.log(Beear.name);
+console.log(Beear.honey);
