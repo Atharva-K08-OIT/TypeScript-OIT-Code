5dfcca76692d61d595b5f992e6ddf2beb9cdebd1
Atharva-K08-OIT <atharvaofficialoit@gmail.com>
2024-09-14 12:12:49 +0530

    Add files via upload

---
+++ 14 Sep/script_14Sep.js
@@ -0,0 +1,27 @@
+// Literal Types
+/*
+// Ex. 2
+
+function compare(a: string, b: string): -1 | 0 | 1 {
+    return a === b ? 0 : a > b ? 1 : -1;
+}
+
+// Ex. 3
+
+function handleRequest(url: string, method: "GET" | "POST"): void{
+    console.log("req accepted.")
+}
+
+var url = "https://example.com";
+
+var method = "GET";
+
+handleRequest(url,method as "GET")
+*/
+// enum Direction {
+//   TOP = "top",
+//   LEFT = "left",
+//   RIGHT = "right",
+//   BOTTOM = "bottom",
+// }
+// console.log(Direction.TOP);
